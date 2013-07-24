e4c6c6fe9e650236e1df5c841c50709f4a7768e2
Sam Starling <sam.starling@gmail.com>
2013-07-24 17:17:53 +0100

    Highlights tag concepts in news pages

--- public/js/inject.js
+++ public/js/inject.js
@@ -5,5 +5,18 @@ $(document).ready(function() {
   $.getJSON("/api/tags?url=" + encoded_url, function(data) {
     console.log(data.about);
     console.log(data.mentions);
+    
+    $.each(data.mentions, function(index, value) {
+      var re = new RegExp(value.guessed_name, 'g');
+      $(".story-body p").each(function( index ) {
+        var html = $(this).html();
+        var span = '<span class="ldp-highlight" ' +
+          'data-dbpedia-uri="' + value.dbpedia_uri + '"' +
+          'data-uri="' + value.uri + '">' +
+          value.guessed_name + '</span>';
+        var new_html = html.replace(re, span);
+        $(this).html(new_html);
+      });
+    });
   });
 });
